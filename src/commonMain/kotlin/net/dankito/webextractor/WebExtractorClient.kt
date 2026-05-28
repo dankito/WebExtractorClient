b3a992f2eab3a74592de28cf9235491e72500cba
dankito <git@dankito.net>
2026-05-28 12:07:07 +0200

    Made serializer configurable as the KotlinxJsonSerializer does not work in Quarkus (whyever)

--- src/commonMain/kotlin/net/dankito/webextractor/WebExtractorClient.kt
+++ src/commonMain/kotlin/net/dankito/webextractor/WebExtractorClient.kt
@@ -5,6 +5,8 @@ import net.dankito.web.client.RequestParameters
 import net.dankito.web.client.WebClient
 import net.dankito.web.client.WebClientResult
 import net.dankito.web.client.post
+import net.dankito.web.client.serialization.KotlinxJsonSerializer
+import net.dankito.web.client.serialization.Serializer
 import net.dankito.webextractor.model.ExtractFromHtmlRequest
 import net.dankito.webextractor.model.ExtractFromHtmlResult
 import net.dankito.webextractor.model.ExtractionRequest
@@ -13,23 +15,30 @@ import net.dankito.webextractor.model.MultiFormatExtractionRequest
 import net.dankito.webextractor.model.MultiFormatExtractionResult
 
 open class WebExtractorClient(
-    private val baseUrl: String,
-    private val webClient: WebClient = Instances.DefaultWebClient
+    protected val baseUrl: String,
+    protected val webClient: WebClient = Instances.DefaultWebClient,
+    protected val serializer: Serializer = KotlinxJsonSerializer(),
 ) {
 
     open suspend fun extract(request: ExtractionRequest): WebClientResult<ExtractionResult> =
-        webClient.post(url("/extract"), request)
+        webClient.post(url("/extract"), serialize(request))
 
     open suspend fun extractFromHtml(request: ExtractFromHtmlRequest): WebClientResult<ExtractFromHtmlResult> =
         webClient.post(RequestParameters(url("/extract/html"), ExtractFromHtmlResult::class,
             request.html, "text/html", "text/markdown", queryParameters = createQueryParameters(request)))
 
     open suspend fun extractMultipleResponseFormats(request: MultiFormatExtractionRequest): WebClientResult<MultiFormatExtractionResult> =
-        webClient.post(url("/extract/multiple"), request)
+        webClient.post(url("/extract/multiple"), serialize(request))
 
 
     protected open fun url(path: String): String =
         "$baseUrl$path"
+
+
+    protected open fun serialize(request: ExtractionRequest): String = serializer.serialize(request)
+
+    protected open fun serialize(request: MultiFormatExtractionRequest): String = serializer.serialize(request)
+
 
     protected open fun createQueryParameters(request: ExtractFromHtmlRequest): Map<String, Any> = buildMap {
         put("format", request.format.name.lowercase())
