a75fa1b240a2cdbe51d7aab5035c17fbbb84ec90
dankito <git@dankito.net>
2026-05-28 10:55:27 +0200

    Converted to Kotlin Multiplatform project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 plugins {
     val kotlinVersion = "2.3.20"
-    kotlin("jvm") version kotlinVersion
+    kotlin("multiplatform") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 }
 
@@ -25,32 +28,99 @@ repositories {
 }
 
 
-val kotlinCoroutinesVersion: String by project
-val kotlinxSerializationVersion: String by project
-val webClientVersion: String by project
-val kmpDateTimeVersion: String by project
+kotlin {
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    compilerOptions {
+        // suppresses compiler warning: [EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING] 'expect'/'actual' classes (including interfaces, objects, annotations, enums, and 'actual' typealiases) are in Beta.
+        freeCompilerArgs.add("-Xexpect-actual-classes")
 
-val assertKVersion: String by project
-val logbackVersion: String by project
-
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
-
-    api("net.dankito.web:web-client-api:$webClientVersion")
-    implementation("net.dankito.web:ktor-web-client:$webClientVersion")
+        // avoid "variable has been optimised out" in debugging mode
+        if (System.getProperty("idea.debugger.dispatch.addr") != null) {
+            freeCompilerArgs.add("-Xdebug")
+        }
+    }
 
 
-    testImplementation(kotlin("test"))
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$kotlinCoroutinesVersion")
+    jvmToolchain(11)
 
-    testImplementation("com.willowtreeapps.assertk:assertk:$assertKVersion")
-    testImplementation("ch.qos.logback:logback-classic:$logbackVersion")
-}
+    jvm()
 
-tasks.test {
-    useJUnitPlatform()
+
+    js {
+        binaries.executable()
+
+        browser {
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    useFirefoxHeadless()
+                }
+            }
+        }
+
+        nodejs {
+            testTask {
+                useMocha {
+                    timeout = "20s" // Mocha times out after 2 s, which is too short for some tests
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser {
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    useFirefoxHeadless()
+                }
+            }
+        }
+    }
+
+
+    linuxX64()
+    mingwX64()
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    macosArm64()
+    watchosArm64()
+    watchosSimulatorArm64()
+    tvosArm64()
+    tvosSimulatorArm64()
+
+    applyDefaultHierarchyTemplate()
+
+
+    sourceSets {
+        val kotlinCoroutinesVersion: String by project
+        val kotlinxSerializationVersion: String by project
+        val webClientVersion: String by project
+
+        val assertKVersion: String by project
+        val logbackVersion: String by project
+
+        commonMain.dependencies {
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${kotlinCoroutinesVersion}")
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${kotlinxSerializationVersion}")
+            api("net.dankito.web:web-client-api:${webClientVersion}")
+            implementation("net.dankito.web:ktor-web-client:${webClientVersion}")
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$kotlinCoroutinesVersion")
+
+            implementation("com.willowtreeapps.assertk:assertk:$assertKVersion")
+        }
+
+        jvmTest.dependencies {
+            implementation("ch.qos.logback:logback-classic:$logbackVersion")
+        }
+    }
 }
 
 
